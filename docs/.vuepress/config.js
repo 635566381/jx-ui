e89912da7002b1d58d35fe51f80279ae413796c5
展白 <hj@juexiaotime.com>
2022-02-13 15:43:11 +0800

    base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
-      { text: 'Github', link: '' },
+      { text: 'Github', link: 'https://github.com/635566381/jx-ui.git' },
       { text: 'VuePress', link: 'https://vuepress.vuejs.org/' },
     ],
     sidebar: ['/', '/componentDocs/card'],
